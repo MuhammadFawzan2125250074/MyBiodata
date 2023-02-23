e9c32c960b650542ca6da4ed3468da1b110f0136
MuhammadFawzan2125250074 <fawzan_mhd@mhs.mdp.ac.id>
2023-02-23 11:18:56 +0700

    Add SharedPreferences
    - SharedPreference for login
    - User can Login and Logout

--- app/src/main/java/com/example/mybiodata/MainActivity.java
+++ app/src/main/java/com/example/mybiodata/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.mybiodata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Utilities utilities = new Utilities();
+    private TextView tvusername;
+    private Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvusername = findViewById(R.id.tv_username);
+        btnLogout = findViewById(R.id.btn_logout);
+
+        if (!utilities.isLogin(this, "xUsername")) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            String username = utilities.getPref(this, "xUsername");
+            tvusername.setText(username);
+        }
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                utilities.setPref(MainActivity.this, "xUsername", null);
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
-}
+    }
